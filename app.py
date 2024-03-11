4972818873873754704fdf7b459d6ec8e9be38fc
lewis wege <wegelewis7@gmail.com>
2024-03-11 17:02:32 +0300

    modified code base to add posts to database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 def get_db_connection():
@@ -34,4 +34,19 @@ def post(post_id):
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('The title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?,?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+        
     return render_template('create.html')
+
